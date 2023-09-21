ecb33ce0a258f0c80760465db43ac2dae69633a0
Stokker <stokkerma@gmail.com>
2023-09-20 19:37:25 -0500

    Eliminados Gets y Sets innecesarios

--- src/main/java/caldas/Estudiante.java
+++ src/main/java/caldas/Estudiante.java
@@ -15,31 +15,6 @@ public class Estudiante {
         return "-Id: "+id+"  -Nombre: "+ nombre+ "  -Apellido: "+apellido;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
     
-
     
 }
